0b41c758bf94c1df23daef92cf61db5baf73924b
SAURABH SHARMA <22629916+itsjzt@users.noreply.github.com>
2018-02-12 16:47:18 +0530

    fixed a bug!

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express')
 const { giveRandomWord } = require('./random')
 const app = express()
 
-const toSend = giveRandomWord()
-app.get('/', (req, res) => res.send(toSend))
+app.get('/', (req, res) => {
+  const toSend = giveRandomWord()
+  res.send(toSend)
+})
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
